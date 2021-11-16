57b33ae84b3859271d12dad4ab2ed5dd42e1a5df
Daniil <dadyarri@gmail.com>
2021-11-16 18:44:27 +0000

    [lw5]: add registering handlers

--- lw5/Program.cs
+++ lw5/Program.cs
@@ -6,8 +6,34 @@ namespace lw5
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+            Book book1 = new Book("Война и мир", "Л. Н. Толстой");
+            book1.RegisterHandler(new Book.Logger(Console.WriteLine));
+
+            Book book2 = new Book("Мастер и Маргарита", "М. А. Булгаков");
+            book2.RegisterHandler(new Book.Logger(Console.WriteLine));
+
+            Magazine mag1 = new Magazine("Ellie");
+            mag1.RegisterHandler(new Magazine.Logger(Console.WriteLine));
+
+            Reader reader1 = new Reader("Голубев Д. О.");
+            reader1.RegisterHandler(new Reader.Logger(Console.WriteLine));
+
+            reader1.TakeBook(book1);
+            reader1.PrintReaderInfo();
+
+            var book2Item = reader1.TakeBook(book2);
+            reader1.PrintReaderInfo();
+
+            reader1.LoseBook(book2Item);
+
+            reader1.PrintReaderInfo();
+
+            reader1.ReturnBook(book1);
+            reader1.PrintReaderInfo();
+
+            reader1.TakeBook(mag1);
+            reader1.PrintReaderInfo();
         }
 
         public static string DateTimeNow()
@@ -19,6 +45,11 @@ namespace lw5
         {
             StackTrace stackTrace = new();
             return stackTrace.GetFrame(1).GetMethod().Name;
+        }
+
+        public void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
         }
     }
 }
