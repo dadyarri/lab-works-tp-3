982ce45c0abdd4fbc6f4382af6372724eb5131a1
Daniil <dadyarri@gmail.com>
2021-11-29 14:00:20 +0000

    style: reorder files

--- lw3/Program.cs
+++ lw3/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-namespace прога
+namespace lw3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Book book1 = new Book();
+            Book book1 = new Book("Война и мир", "Л. Н. Толстой");
+            Book book2 = new Book("Мастер и Маргарита", "М. А. Булгаков");
+            Magazine mag1 = new Magazine("Ellie");
+
+            Reader reader1 = new Reader("Голубев Д. О.");
+            reader1.TakeBook(book1);
+            reader1.PrintReaderInfo();
+
+            var book2Item = reader1.TakeBook(book2);
+            reader1.PrintReaderInfo();
+
+            reader1.LoseBook(book2Item);
+
+            reader1.PrintReaderInfo();
+
+            reader1.ReturnBook(book1);
+            reader1.PrintReaderInfo();
+
+            reader1.TakeBook(mag1);
+            reader1.PrintReaderInfo();
         }
     }
 }
