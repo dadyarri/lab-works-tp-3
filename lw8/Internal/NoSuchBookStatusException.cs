9237cc812f8d694d2c5917397c99e9febb082929
Daniil <dadyarri@gmail.com>
2021-12-21 16:05:36 +0000

    add 8th work

---
+++ lw8/Internal/NoSuchBookStatusException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace lw8
+{
+    class NoSuchBookStatusException : Exception
+    {
+        public NoSuchBookStatusException(string message) : base(message) { }
+    }
+}
---
+++ lw8/LibraryCard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lw8
+{
+    /// <summary>
+    /// Читательский билет
+    /// <param name="Books">Список из <c>LibraryCardItem</c>, в котором хранится история взятых и возвращенных книг/журналов</param>
+    /// </summary>
+    public class LibraryCard
+    {
+        Logger log;
+
+        public void RegisterHandler(Logger handler)
+        {
+            log += handler;
+        }
+
+        public void UnregisterHandler(Logger handler)
+        {
+            log -= handler;
+        }
+
+        public LibraryCard()
+        {
+            Books = new List<LibraryCardItem>();
+        }
+
+        public List<LibraryCardItem> Books { get; set; }
+
+        public override string ToString()
+        {
+            return $"<{string.Join(", ", Books)}>";
+        }
+    }
+}
